3ec24b5a8990f0ca1fd957428d5a9d93032c456e
karanp1 <karanp1@gmail.com>
2016-12-20 01:11:21 +0530

    Edit 1

---
+++ app.js
@@ -0,0 +1,16 @@
+var r = [1,10,9];
+var sum1 = 0;
+var sum2 = 0;
+for (i = 0; i < 1001; i++) { 
+    sum1 = sum1 + i;
+}
+
+
+
+console.log(sum1);
+var j = 0;
+while(j<1000){
+	j = j + 1
+	sum2 = sum2 + j
+}
+console.log(sum2)
